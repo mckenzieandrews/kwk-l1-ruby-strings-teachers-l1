d3670ea82f352d4a5256918c6b1f923df5cf036d
McKenzie Andrews <c.mckenzie.a@gmail.com>
2018-06-27 17:04:30 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -18,7 +18,7 @@ host_name = response5
 # Try starting out with puts'ing a string.
 puts "Dear #{guest_name},"
 
-puts "You are cordially invited to the #{party_name} on #{date} at #{time}. Please RSVP no later than November 10th."
+puts "You are cordially invited to the #{party_name} on #{date} at #{time}. Please RSVP no later than December 10th."
 
 puts "Sincerely,"
 
