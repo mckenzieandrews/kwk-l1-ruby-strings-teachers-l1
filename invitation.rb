8a536b0042c760a476705bd333c76340a7a9d4ae
McKenzie Andrews <c.mckenzie.a@gmail.com>
2018-06-27 17:05:04 +0000

    automatically backed up by learn

--- invitation.rb
+++ invitation.rb
@@ -20,6 +20,6 @@ puts "Dear #{guest_name},"
 
 puts "You are cordially invited to the #{party_name} on #{date} at #{time}. Please RSVP no later than December 10th."
 
-puts "Sincerely,"
+puts "Best,"
 
 puts "#{host_name}"
